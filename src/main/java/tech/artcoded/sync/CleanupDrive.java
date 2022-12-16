7a6f7304255fd7ad795c6d7bcbca33c0d908f81a
Nordine Bittich <nordine@bittich.be>
2022-12-16 23:58:40 +0100

    cleanup

---
+++ src/main/java/tech/artcoded/sync/CleanupDrive.java
@@ -0,0 +1,33 @@
+package tech.artcoded.sync;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CleanupDrive {
+  private final DriveService driveService;
+
+  public CleanupDrive(DriveService driveService) {
+    this.driveService = driveService;
+  }
+
+  @Value("${application.cleanup.drive")
+  private boolean cleanupDrive;
+
+  @PostConstruct
+  public void cleanup() throws IOException {
+    // cleanup when drive is full
+    if (cleanupDrive) {
+        var drive = this.driveService.getDrive();
+        var files = drive.files().list().execute();
+        for(var fileId: files.keySet()) {
+          drive.files().delete(fileId).execute();
+        }
+        drive.files().emptyTrash().execute();
+    }
+  }
+}
