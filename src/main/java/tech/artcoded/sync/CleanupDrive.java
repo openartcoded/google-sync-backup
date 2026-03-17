e374787566de496d1823d09eada885c211a9c244
Nordine Bittich <contact@bittich.be>
2026-03-18 00:11:28 +0100

    fix root cause empty

--- src/main/java/tech/artcoded/sync/CleanupDrive.java
+++ src/main/java/tech/artcoded/sync/CleanupDrive.java
@@ -29,20 +29,22 @@ public class CleanupDrive {
   public void cleanup() throws IOException {
     // cleanup when drive is full
     if (cleanupDrive) {
-        var drive = this.driveService.getDrive();
-        FileList files = drive.files().list().execute();
-        log.info("files being deleted: {}",files);
-        List<File> gFiles = files.getFiles();
-        for(var gFile: gFiles) {
-          try {
-            drive.files().delete(gFile.getId()).execute();
-            log.info("deleted {}",gFile.getId());
-          }catch(Exception e) {
-            log.error("error {}", e.getCause());
-          }
+      var drive = this.driveService.getDrive();
+      FileList files = drive.files().list()
+          .setIncludeItemsFromAllDrives(true)
+          .setFields("files(id,name)").execute();
+      log.info("files being deleted: {}", files);
+      List<File> gFiles = files.getFiles();
+      for (var gFile : gFiles) {
+        try {
+          drive.files().delete(gFile.getId()).setSupportsAllDrives(true).execute();
+          log.info("deleted {}", gFile.getId());
+        } catch (Exception e) {
+          log.error("error {}", e.getMessage());
         }
-        drive.files().emptyTrash().execute();
-        log.info("done");
+      }
+      drive.files().emptyTrash().execute();
+      log.info("done");
     }
   }
 }
